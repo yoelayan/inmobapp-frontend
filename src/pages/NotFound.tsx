5d056b76d90226d6f401ea436ae410e3b81c1936
Yoel Ayan <yoeldategeek@gmail.com>
2025-09-02 23:37:21 +0100

    Refactor NotFound component and remove unused useTab hook
    - Simplified the NotFound component by removing unused imports and variables, enhancing readability and performance.
    - Deleted the useTab hook as it was no longer needed, streamlining the codebase.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,38 +4,12 @@
 import Link from 'next/link'
 
 // MUI Imports
-import useMediaQuery from '@mui/material/useMediaQuery'
-import { styled, useTheme } from '@mui/material/styles'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 
-// Third-party Imports
-import classnames from 'classnames'
-
-// Type Imports
-
-// Hook Imports
-import { useImageVariant } from '@core/hooks/useImageVariant'
-
-// Styled Components
-const MaskImg = styled('img')({
-  blockSize: 'auto',
-  maxBlockSize: 355,
-  inlineSize: '100%',
-  position: 'absolute',
-  insetBlockEnd: 0,
-  zIndex: -1
-})
-
 const NotFound = () => {
-  // Vars
-  const darkImg = '/images/pages/misc-mask-dark.png'
-  const lightImg = '/images/pages/misc-mask-light.png'
 
   // Hooks
-  const theme = useTheme()
-  const hidden = useMediaQuery(theme.breakpoints.down('md'))
-  const miscBackground = useImageVariant('light', lightImg, darkImg)
 
   return (
     <div className='flex items-center justify-center min-bs-[100dvh] relative p-6 overflow-x-hidden'>
@@ -56,13 +30,7 @@ const NotFound = () => {
           className='object-cover bs-[400px] md:bs-[450px] lg:bs-[500px] mbs-10 md:mbs-14 lg:mbs-20'
         />
       </div>
-      {!hidden && (
-        <MaskImg
-          alt='mask'
-          src={miscBackground}
-          className={classnames({ 'scale-x-[-1]': theme.direction === 'rtl' })}
-        />
-      )}
+
     </div>
   )
 }
